93d8d0d722ade94829766b845ea0f8f568325fd2
Karnardia Simpson <karnardia.simpson@mymona.uwi.edu>
2025-11-14 16:12:27 -0500

    Added images

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 /* Add your JavaScript to this file */
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    const form =document.querySelector('form');
+    const msg = document.querySelector('.message');
+    const einput = document.getElementById('email');
+
+
+    form.addEventListener ('submit',function(event){
+        event.preventDefault();
+        const email = einput.value.trim();
+        
+        if (email !==''){
+            msg.textContent = `Thank you! Your email address ${email} has been added to our mailing list!` ;
+            einput.value = '';
+        }else{
+            msg.textContent = 'Please enter a valid email address.';
+    }
+});
+});
